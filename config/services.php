6b7069b4e1b083e5cd8da38671a3f7859c7e6f7c
Zakiullah Safi <zakiullahsafi00@gmail.com>
2025-08-11 22:15:16 +0430

    updated the services.php

--- config/services.php
+++ config/services.php
@@ -1,38 +1,24 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+        'token' => env('POSTMARK_TOKEN', null), // Add default null
     ],
 
     'resend' => [
-        'key' => env('RESEND_KEY'),
+        'key' => env('RESEND_KEY', null), // Add default null
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'key' => env('AWS_ACCESS_KEY_ID', null), // Add default null
+        'secret' => env('AWS_SECRET_ACCESS_KEY', null), // Add default null
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN', null), // Add default null
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL', null), // Add default null
         ],
     ],
-
 ];
